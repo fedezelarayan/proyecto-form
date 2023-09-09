e771c6925565c63b7e3dfe6e477b36adb09d949a
Federico Zelarayan <federico.zelarayan@hotmail.com>
2023-09-08 23:27:45 -0300

    first commit client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+        <a>
+          aloooo
+        </a>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    surveys: []
+}
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        default:
+            return {...state};
+    }
+    }
+    
+    export default rootReducer;
